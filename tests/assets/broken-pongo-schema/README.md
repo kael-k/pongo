717bab13e29537ef98a2337f8213e323f669f05c
D'Alcamo Kael <dalcamokael@gmail.com>
2022-12-04 21:13:57 +0100

    tests: added some tests on for broken marshaled PonGO Schema

---
+++ tests/assets/broken-pongo-schema/README.md
@@ -0,0 +1,3 @@
+These files are all JSON PonGO schema which are all broken, for one of this two cases:
+* wrong JSON syntax
+* invalid schema
---
+++ tests/assets_broken_pongo_schema_test.go
@@ -0,0 +1,42 @@
+package tests
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/kael-k/pongo/pongo"
+)
+
+// this test file aims to expose all tests file in tests/assets
+// and to validate the schemas inside it
+
+const testRootBrokenPongoSchemas = "../tests/assets/broken-pongo-schema"
+
+func TestBrokenPongoSchemas(t *testing.T) {
+	testsDir, err := os.ReadDir(testRootBrokenPongoSchemas)
+
+	if err != nil {
+		t.Errorf("cannot test broken schemas, error on read %s: %s", testRootBrokenPongoSchemas, err)
+		return
+	}
+
+	for _, testFile := range testsDir {
+		filename := testFile.Name()
+		if filename == "README.md" {
+			continue
+		}
+
+		path := fmt.Sprintf("%s/%s", testRootBrokenPongoSchemas, testFile.Name())
+		rawPonGOJSONSchema, err := os.ReadFile(path)
+		if err != nil {
+			t.Errorf("cannot test broken schemas, error on read %s: %s", path, err)
+			return
+		}
+
+		_, _, err = pongo.UnmarshalSchemaJSON(rawPonGOJSONSchema)
+		if err == nil {
+			t.Errorf("expected error on path %s, got no error", path)
+		}
+	}
+}
